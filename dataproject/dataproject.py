537617e6d0294bed044c3766a906b30e91853c49
dtf747 <dtf747@alumni.ku.dk>
2024-04-14 16:37:45 +0200

    Final dataproject

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,10 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
+def plot_interactive(ax: plt.Axes, x: list, y: list, title: str, xlabel: str, ylabel: str):
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
-
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+    # Create a line plot with a trend
+    ax.scatter(x, y)
+    sns.regplot(x = x, y = y, scatter = False, ax = ax)
+   
+    # Set the title and the labels for the axis
+    ax.set_title(title, fontsize = 12)
+    ax.set_xlabel(xlabel, fontsize = 12)
+    ax.set_ylabel(ylabel, fontsize = 12)
