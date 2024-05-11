8176df05ddc4a9e07efff0d8ceed65483687c508
dtf747 <dtf747@alumni.ku.dk>
2024-05-11 16:08:21 +0200

    changes_1

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,82 +1,45 @@
-from types import SimpleNamespace
+# Imports required to read the code
 import numpy as np
 from scipy import optimize
+import sympy as sm
+from sympy import *
+import random
 
-class SolowClass():
-
-    def __init__(self,do_print=True):
-        """ create the model """
-
-        if do_print: print('initializing the model:')
-
-        self.par = SimpleNamespace()
-        self.ss = SimpleNamespace()
-        self.path = SimpleNamespace()
-
-        if do_print: print('calling .setup()')
-        self.setup()
-
-    
-    def setup(self):
-        """ baseline parameters """
-
-        par = self.par
-
-        par.s_K = 2.0 # Saving rate physical capital
-        par.s_H = 0.1 # Saving rate human capital
-        par.g = 0.02 # Growth rate in technology
-        par.n = 0.01 # Growth rate in population
-        par.alpha = 1/3 # capital weight  
-        par.phi = 1/3 #     
-        par.delta = 0.05 # depreciation rate
-        par.production_function = 'cobb-douglas'
-
-        # c. initial
-        par.K_lag_ini = 1.0
-        par.L_lag_ini = 1.0
-        par.A_lag_ini = 1.0
-        par.Y_lag_ini = 1.0
-
-        # d. misc
-        par.solver = 'broyden' # solver for the equation system, 'broyden' or 'scipy'
-        par.Tpath = 500 # length of transition path, "truncation horizon"
-        
-    def solow_equations(vars , s_K, s_H, n , g, delta, alpha, phi):
-
-        k_tilde , h_tilde = vars
-
-        solow_k = (s_K * k_tilde**alpha * h_tilde**phi - (delta + n + g + n*g)*k_tilde)/((1+n)*(1+g))
-        solow_h = (s_H * k_tilde**alpha * h_tilde**phi - (delta + n + g + n*g)*h_tilde)/((1+n)*(1+g))
-
-        return solow_k , solow_h
-
-
-
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
-
-    Args:
-        c (float): costs
-        alpha (float): parameter
-
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
-
-    """ 
-    
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
-
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+# Step 1: Initialize the baseline parameters 
+s_K = 0.15
+s_H = 0.10
+g = 0.02
+n = 0.01
+alpha = 1/3
+phi = 1/3
+delta = 0.075
 
 
 def solow_equations(vars , s_K, s_H, n , g, delta, alpha, phi):
+
     k_tilde , h_tilde = vars
+
     solow_k = (s_K * k_tilde**alpha * h_tilde**phi - (delta + n + g + n*g)*k_tilde)/((1+n)*(1+g))
     solow_h = (s_H * k_tilde**alpha * h_tilde**phi - (delta + n + g + n*g)*h_tilde)/((1+n)*(1+g))
+
     return solow_k , solow_h
 
+
+def ss_solve(num_guess = 500, bounds=[-1000, 1000], fun=solow_equations, args = None , method='hybr'):
+
+        smallest_res = np.inf
+
+        random_samples = list(np.random.uniform(low=bounds[0],high=bounds[1], size=num_guess))
+
+        for i in range(num_guess):
+            initial_guess=random.sample(random_samples, 2)
+
+            solution = optimize.root(fun = fun, x0=initial_guess, args=args, method=method)
+
+            residual_norm = np.linalg.norm(solution.fun)
+
+            if residual_norm<smallest_res:
+                smallest_res=residual_norm
+                ss_k , ss_h = solution.x
+        
+        return ss_k, ss_h, smallest_res
