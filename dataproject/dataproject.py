0ba8717027ff1351c50efbfe9a2ca47b57dd8b62
dtf747 <dtf747@alumni.ku.dk>
2024-06-18 10:56:00 +0200

    Final changes

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,10 +1,40 @@
-def plot_interactive(ax: plt.Axes, x: list, y: list, title: str, xlabel: str, ylabel: str):
+from types import SimpleNamespace
+import pandas as pd
 
-    # Create a line plot with a trend
-    ax.scatter(x, y)
-    sns.regplot(x = x, y = y, scatter = False, ax = ax)
-   
-    # Set the title and the labels for the axis
-    ax.set_title(title, fontsize = 12)
-    ax.set_xlabel(xlabel, fontsize = 12)
-    ax.set_ylabel(ylabel, fontsize = 12)
+class data_project:
+    
+    def __init__(self, eco_api):
+        self.eco_api = eco_api
+
+    def calculate_percentage_changes(self):
+        # Create an empty DataFrame to store the results
+        percentage_changes = pd.DataFrame()
+
+        # Define the period
+        years = range(2004, 2023) 
+
+        # Loop through each year and create the variable current_year_values and next_year_values
+        for TID in years:
+            if TID + 1 in self.eco_api.index:
+                current_year_values = self.eco_api.loc[self.eco_api.index == TID, 'INDHOLD'].astype(float)
+                next_year_values = self.eco_api.loc[self.eco_api.index == TID + 1, 'INDHOLD'].astype(float)
+
+                # Calculate the percentage change for each variable
+                change = (next_year_values.values - current_year_values.values) / current_year_values.values * 100
+
+                # Create a new column in the results DataFrame for each year
+                percentage_changes[f'{TID+1}'] = change
+
+        # Rename the index-column to the true variable names
+        Variables_names = ['Total_Turn_Over', 'Total_Meat', 'Total_Dairy', 'Total_Fruits', 'Total_Vegetables']
+        percentage_changes.index = Variables_names
+
+        # Transpose the dataframe so the years become the index column
+        percentage_changes_transposed = percentage_changes.T
+        percentage_changes_transposed = percentage_changes_transposed.rename_axis('TID')
+
+        # Transform index column to numeric values
+        percentage_changes_transposed.index = pd.to_numeric(percentage_changes_transposed.index)
+
+
+  
